28b95de48b8d0aba852e13f83dd8ca2c85e008c3
satoshishi <s9r6nbij9@gmail.com>
2021-07-15 17:35:42 +0900

    ContainerにDisposeを追加

--- main/Packages/ZaContainer/Runtime/Core/ZaContainer.cs
+++ main/Packages/ZaContainer/Runtime/Core/ZaContainer.cs
@@ -8,7 +8,7 @@ using System.Linq;
 
 namespace ZaCo.Core
 {
-    public class ZaContainer
+    public class ZaContainer : IDisposable
     {
         public static ZaContainer Create() => new ZaContainer();
 
@@ -39,23 +39,23 @@ namespace ZaCo.Core
             }
 
             if (instance == null) Debug.LogWarning($"Locator: {typeof(T).Name} not found.");
- 
+
             return instance;
-        }        
+        }
 
         private class ReceiverMethods
         {
             public object services;
 
             public IEnumerable<MethodInfo> infos;
-        }        
-        
+        }
+
         public ReadonlyZaContainer Build()
         {
             var readOnly = CreateReadony();
             var receiverMethods = GetTargetMethodFromInstanceDict();
 
-            foreach(ReceiverMethods methods in receiverMethods)
+            foreach (ReceiverMethods methods in receiverMethods)
                 NotifyToReceiverMethods(methods);
 
             return readOnly;
@@ -63,19 +63,19 @@ namespace ZaCo.Core
 
         private void NotifyToReceiverMethods(ReceiverMethods methods)
         {
-            foreach(MethodInfo info in methods.infos)
-                info.Invoke(methods.services,new object[]{CreateReadony()});
+            foreach (MethodInfo info in methods.infos)
+                info.Invoke(methods.services, new object[] { CreateReadony() });
         }
 
         private List<ReceiverMethods> GetTargetMethodFromInstanceDict()
         {
             List<ReceiverMethods> methods = new List<ReceiverMethods>();
 
-            foreach(object services in instanceDict.Values)
+            foreach (object services in instanceDict.Values)
             {
-                 var targetMethods = services.GetType().GetMethods().Where(scirpt => scirpt.GetCustomAttributes(typeof(ReceiveZaContainerAttribute), false).Any());
+                var targetMethods = services.GetType().GetMethods().Where(scirpt => scirpt.GetCustomAttributes(typeof(ReceiveZaContainerAttribute), false).Any());
 
-                 if(targetMethods != null && targetMethods.Count() > 0)
+                if (targetMethods != null && targetMethods.Count() > 0)
                     methods.Add(new ReceiverMethods()
                     {
                         services = services,
@@ -84,6 +84,12 @@ namespace ZaCo.Core
             }
 
             return methods;
+        }
+
+        public void Dispose()
+        {
+            instanceDict.Clear();
+            instanceDict = new Dictionary<Type, object>();
         }
     }
 }
